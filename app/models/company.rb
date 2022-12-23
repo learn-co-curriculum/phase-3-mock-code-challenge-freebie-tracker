37e7b602e809ae28b3f16f78b6d2526bfa4dad77
Cecilia Milla <cmilla1020@gmail.com>
2022-12-23 10:53:11 -0500

    Revert "adding my solutions"
    This reverts commit e0cdaa581d340c44de25fe2b541bd6bd905e05d3.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,11 +1,3 @@
 class Company < ActiveRecord::Base
-  has_many :freebies
-  has_many :devs, through: :freebies
 
-  def give_freebie(dev, item_name, value)
-    Freebie.create(item_name: item_name, item_value: value, dev_id: dev.id, company_id: self.id)
-  end
-  def self.oldest_company
-    Company.all.order(:founding_year).first
-  end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,13 +1,3 @@
-rake class Dev < ActiveRecord::Base
-    has_many :freebies
-    has_many :companies, through: :freebies
+class Dev < ActiveRecord::Base
 
-    def received_one?(item_name)
-        self.freebies.any? do |freebie|
-            freebie.item_name == item_name
-        end
-    end
-    def give_away(freebie, dev)
-        freebie.update(dev: dev) unless freebie.dev !=self
-    end
 end
